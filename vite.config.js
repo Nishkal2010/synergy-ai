c68ec02d13fd5ced8faefe30ada865f705ba8e9a
Nishkal2010 <nishkal.dachepelly@gmail.com>
2025-12-14 18:35:36 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
